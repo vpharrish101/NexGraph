edf6c9956575166395fa45362390bf6d3d29e36f
Vp Harrish <74062489+vpharrish101@users.noreply.github.com>
2025-12-13 01:30:36 +0530

    Update main_orch.py

--- app/main_orch.py
+++ app/main_orch.py
@@ -20,24 +20,23 @@ def create_graph(graph:PyD_models.GraphDef):
     return {"graph_id":g_id,"message":"Graph stored successfully"}
 
 @app.websocket("/ws/run/{graph_id}")
-async def run_workflow_socket(websocket: WebSocket, graph_id: str):
+async def run_workflow_socket(websocket: WebSocket,graph_id: str):
     await websocket.accept()
 
-    # Graph MUST exist on server
+
     if graph_id not in graphs:
-        await websocket.send_json({"error": "Graph not found"})
+        await websocket.send_json({"error":"Graph not found"})
         await websocket.close()
         return
 
-    # Only receive STATE from client (no graph!)
-    data = await websocket.receive_json()
-    initial_state = data.get("state", {})
+    data=await websocket.receive_json()
+    initial_state=data.get("state",{})
 
-    graph_def = graphs[graph_id]
+    graph_def=graphs[graph_id]
 
-    async def stream_log(node_id, current_state):
-        preview = current_state.get("current_summary", "")
-        preview = (preview[:50] + "...") if len(preview) > 50 else preview
+    async def stream_log(node_id,current_state):
+        preview=current_state.get("current_summary","")
+        preview=(preview[:50]+"...") if len(preview)>50 else preview
 
         await websocket.send_json({
             "step": node_id,
@@ -45,8 +44,9 @@ async def run_workflow_socket(websocket: WebSocket, graph_id: str):
             "action": current_state.get("action_taken", "thinking")
         })
 
-    engine_s = engine.GraphFLow_Engine(graph_def, callback=stream_log)
-    final_state = await engine_s.run(initial_state)
+    engine_s=engine.GraphFLow_Engine(graph_def,callback=stream_log)
+    final_state await engine_s.run(initial_state)
 
-    await websocket.send_json({"status": "complete", "final_state": final_state})
+    await websocket.send_json({"status":"complete","final_state":final_state})
     await websocket.close()
+
